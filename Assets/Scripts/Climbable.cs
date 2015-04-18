c1f65c1c78c06b61b55bed4289ff543dd7c7346e
stephentrinh <s6trinh@ACS-CSEB270-20.ad.ucsd.edu>
2015-04-17 23:20:21 -0700

    A trigger collider example.

---
+++ Assets/Scripts/Climbable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Attached to a trigger collider to set the player's CanClimb to work.
+public class Climbable : MonoBehaviour 
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    { 
+        var player = other.GetComponent<Platformer2DUserControl>();
+
+        if (player == null)
+        {
+            return;
+        }
+
+        player.CanClimb();
+    }
+}
