ff6526629b4b5105711eca3ed79d6abeb4c19e73
Dexter Friedman <drfriedm@ucsd.edu>
2015-04-18 20:12:41 -0700

    Bear AI is bad, and I should feel bad. (I'm sorry) Also, LaserBear prefab is updated to have decent colliders

---
+++ Assets/Scripts/BearHearingRadius.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BearHearingRadius : MonoBehaviour {
+
+	public BearController bc;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		string otherTag = other.gameObject.tag;
+		if (otherTag == "Player") {
+			if ( Input.anyKey ) {
+				// If the player is pressing any key (not pressing no keys)
+				// then the bear can hear you
+				bc.AlertBear();
+			}
+		}
+	}
+}
